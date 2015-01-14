0c8d359b6a2180f713836df5c7360d34c015f9be
Christopher Lee <akachrislee@gmail.com>
2015-01-14 03:46:38 -0500

    combining find and kill makes the code simpler

--- tab-killer.js
+++ tab-killer.js
@@ -2,20 +2,36 @@ var TabKiller = function(url){
   this.url = url;
 }
 
-TabKiller.prototype.find_all_tabs_with_url = function() {
+TabKiller.prototype.kill = function() {
+  this.find_and_kill_tabs_with_url(this.url);
+}
+
+
+TabKiller.prototype.find_and_kill_tabs_with_url = function() {
   var query_info = {
-    url:['http://www.reddit.com/', 'http://reddit.com/']
+    url:[
+      'https://www.'+this.url+'/',
+      'http://www.'+this.url+'/',
+      'https://'+this.url+'/',
+      'http://'+this.url+'/'
+    ]
   }
 
   chrome.tabs.query(query_info, function(tabs){
-    chrome.tabs.remove( _.map(tabs, function(tab){ return tab.id; }) );
-  })
+    _.each(tabs, function(tab){
+      chrome.tabs.remove(tab.id);
+   });
+  });
 };
-
 
 $(document).ready(function(){
   $('.kill-reddit-js').on('click', function(e){
     var tk = new TabKiller('reddit.com');
-    tk.find_all_tabs_with_url();
+    tk.kill();
+  });
+
+  $('.kill-hn-js').on('click', function(e){
+    var tk = new TabKiller('news.ycombinator.com');
+    tk.kill();
   });
 });
