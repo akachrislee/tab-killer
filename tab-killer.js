819ecea7958491b8248698f6e75fed943dc6d618
Christopher Lee <akachrislee@gmail.com>
2015-01-14 04:00:02 -0500

    add a count of tabs for a reddit and hn

--- tab-killer.js
+++ tab-killer.js
@@ -2,12 +2,31 @@ var TabKiller = function(url){
   this.url = url;
 }
 
-TabKiller.prototype.kill = function() {
+TabKiller.get_url_counts = function(url){
+  var query_info = {
+    url:[
+      'https://www.'+url+'/',
+      'http://www.'+url+'/',
+      'https://'+url+'/',
+      'http://'+url+'/'
+    ]
+  }
+
+  chrome.tabs.query(query_info, function(tabs){
+    if (url === 'reddit.com'){
+      $('#reddit-count').html(tabs.length);
+    } else if (url === 'news.ycombinator.com'){
+      $('#hn-count').html(tabs.length);
+    } else {}
+  });
+}
+
+TabKiller.prototype.kill = function(){
   this.find_and_kill_tabs_with_url(this.url);
 }
 
 
-TabKiller.prototype.find_and_kill_tabs_with_url = function() {
+TabKiller.prototype.find_and_kill_tabs_with_url = function(){
   var query_info = {
     url:[
       'https://www.'+this.url+'/',
@@ -18,13 +37,16 @@ TabKiller.prototype.find_and_kill_tabs_with_url = function() {
   }
 
   chrome.tabs.query(query_info, function(tabs){
-    _.each(tabs, function(tab){
-      chrome.tabs.remove(tab.id);
+   _.each(tabs, function(tab){
+     chrome.tabs.remove(tab.id);
    });
   });
 };
 
 $(document).ready(function(){
+  TabKiller.get_url_counts('reddit.com');
+  TabKiller.get_url_counts('news.ycombinator.com');
+
   $('.kill-reddit-js').on('click', function(e){
     var tk = new TabKiller('reddit.com');
     tk.kill();
@@ -33,5 +55,6 @@ $(document).ready(function(){
   $('.kill-hn-js').on('click', function(e){
     var tk = new TabKiller('news.ycombinator.com');
     tk.kill();
+    TabKiller.get_url_counts('news.ycombinator.com');
   });
 });
